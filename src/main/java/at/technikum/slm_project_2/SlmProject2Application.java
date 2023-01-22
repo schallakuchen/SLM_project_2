36e8c01d5e202cde39aade1a564a16035fa8baf8
schallakuchen <andreas.schaller7@gmail.com>
2023-01-22 17:50:51 +0100

    Initial commit

---
+++ src/main/java/at/technikum/slm_project_2/SlmProject2Application.java
@@ -0,0 +1,13 @@
+package at.technikum.slm_project_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmProject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/slm_project_2/SlmProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.slm_project_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
